8ab49c591902ed5c0266e56ee20fea2adf052c72
SerdarAkbulut <AkbulutSerdar@outlook.com.tr>
2025-01-23 11:12:39 +0300

    ...

---
+++ app/components/header/header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <div className="bg-gray-500 text-white p-4 flex justify-between">
+        <div>asdsas</div>
+        <div>Search</div>
+        <div>giriş</div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="bg-red-500 flex h-4">
+      <nav className="navbar navbar-expand-lg navbar-light bg-slate-500" />
+    </div>
+  );
+};
+export default Navbar;
